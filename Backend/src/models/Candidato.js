a266caeead12163e2ad5f5101afa19934ddeebc3
Fernando Moreti <fernandombolela@gmail.com>
2025-08-03 12:40:26 -0300

    Adicionando o model Candidato e consequentemente Endereço

---
+++ Backend/src/models/Candidato.js
@@ -0,0 +1,41 @@
+const { DataTypes, UUID } = require("sequelize")
+const sequelize = require("../config/database")
+
+const Candidato = sequelize.define("Candidato", {
+    id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true        
+    },
+    nome: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    dataNascimento: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    id_formulario: {
+        type: DataTypes.UUID,
+        allowNull: false,
+        references: {
+            model: "tb_formulario",
+            key: "id"
+        },
+        onUpdate: "CASCADE",
+        onDelete: "CASCADE"
+    },
+    id_endereco: {
+        type: DataTypes.UUID,
+        allowNull: false,
+        references: {
+            model: "tb_endereco",
+            key: "id"
+        },
+        onUpdate: "CASCADE",
+        onDelete: "SET NULL"
+    },
+}, {
+    tableName: "tb_candidato",
+    timestamps: true,
+})
