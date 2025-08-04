289499a32ac8b13f45e0dfe222a16726aacbb2d2
Fernando Moreti <fernandombolela@gmail.com>
2025-08-03 22:17:04 -0300

    Adicionando o controller do Formulario

--- Backend/src/controllers/FormularioController.js
+++ Backend/src/controllers/FormularioController.js
@@ -3,15 +3,57 @@ const Formulario = require('../models/Fomulario');
 class FormularioController {
 
     async index(request, response) {
-        const formularios = await Formulario.findAll();
-        return response.json(formularios);
+        try {
+            const formularios = await Formulario.findAll();
+            return response.json(formularios);
+        } catch (error) {
+            return response.status(500).json({ error: "Erro ao buscar Formularios" })
+        }
+    }
+
+    async show(request, response) {
+        const { id } = request.params
+        try {
+            const formulario = await Formulario.findByPk(id)
+            if (!formulario) {
+                return response.status(404).json({ error: "Formulario não encontrado" })
+            }
+            return response.json(formulario)
+        } catch (error) {
+            return response.status(500).json({ error: "Erro ao buscar por formulario"})
+        }
     }
 
     async create(request, response) {
-        const formulario = await Formulario.create(request.body)
-        return response.json(formulario)
+        try {
+            const formulario = await Formulario.create(request.body)
+            return response.json(formulario)
+        } catch (error) {
+            return response.status(500).json({ error: "Erro ao criar Formulario"})
+        }
     }
-
+    
+    async update(request, response) {
+        try {
+            const formulario = await Formulario.update(request.body)
+            return response.json(formulario)
+        } catch (error) {
+            return response.status(500).json({ error: "Erro ao atualizar Formulario"})
+        }
+    }
+    
+    async delete(request, response) {
+        try {
+            const formulario = await Formulario.findByPk(request.parms);
+            if (!formulario) {
+                return response.status(404).json({ error: 'Formulario não encontrado.' });
+            }
+            await formulario.destroy();
+            return response.status(204).send();
+        } catch (error) {
+            return res.status(500).json({ error: 'Erro ao deletar Formulario.' });
+        }
+    }
 }
 
 module.exports = new FormularioController()
