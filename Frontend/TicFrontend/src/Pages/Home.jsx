b5e7ad851889b6a4b67db353203ed6f903d3fdb7
Fernando Moreti <fernandombolela@gmail.com>
2025-08-21 22:06:39 -0300

    Adicionando um fetch

--- Frontend/TicFrontend/src/Pages/Home.jsx
+++ Frontend/TicFrontend/src/Pages/Home.jsx
@@ -1,10 +1,25 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
+import "../Styles/home.css"
+import { Link } from "react-router-dom"
 
 function Home() {
+    const [acessibilidade, setAcessibilidade] = useState([]);
+
+    useEffect(() => {
+    async function fetchData() {
+      const res = await fetch("http://localhost:8000/acessibilidade");
+      const data = await res.json();
+      console.log(data)
+      setAcessibilidade(data);
+    }
+    fetchData();
+  }, []);
+
     return (
-        <>
-            <h1>Ola home</h1>
-        </>
+        <section className="flex flex-col gap-10">
+            <h1 className="border-2 p-4 rounded-4xl animate-pulse shadow-xl shadow-blue-800">Ola home</h1>
+            <Link className="border-2 p-2 rounded-4xl shadow-xl text-center hover:bg-blue-950 duration-500" to={"/vagas"}> Vagas </Link>
+        </section>
     )
 }
 
