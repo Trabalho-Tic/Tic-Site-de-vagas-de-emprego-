540b325f8d90b18983c06667563043fa3fdfbebd
Fernando Moreti <fernandombolela@gmail.com>
2025-08-26 00:36:43 -0300

    Criando o login

--- Frontend/TicFrontend/src/Pages/Login.jsx
+++ Frontend/TicFrontend/src/Pages/Login.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react"
 import "../Styles/home.css"
 import { Link } from "react-router-dom"
 import Input from "../components/input";
+import photoLogin from "../assets/photologin.png"
 
 function Login() {
     const [acessibilidade, setAcessibilidade] = useState([]);
@@ -19,10 +20,27 @@ function Login() {
     }, []);
 
     return (
-        <section className="flex flex-col gap-10">
-            <h1 className="border-2 p-4 rounded-4xl animate-pulse shadow-xl shadow-blue-800">Ola home</h1>
-            <Link className="border-2 p-2 rounded-4xl shadow-xl text-center hover:bg-blue-950 duration-500" to={"/vagas"}> Vagas </Link>
-            <Input></Input>
+        <section className="flex justify-center items-center">
+            <div className="flex flex-col w-125 h-150 border-1 rounded-xl justify-center p-10">
+                <div>
+                    <h1 className="!text-2xl pb-8">Welcome !</h1>
+                </div>
+                <div>
+                    <h2 className="text-3xl font-bold">Sign in to</h2>
+                    <p className="text-sm pb-12">Lorem ipsum dolor</p>
+                </div>
+                <div className="pb-12">
+                    <p className="pb-2">User name</p>
+                    <Input placeholder="Enter your user name"></Input>
+                    <p className="pb-2">Password</p>
+                    <Input placeholder="Enter your password"></Input>
+                </div>
+                <div className="pb-8">
+                    <button className="!bg-black border-2 px-6 rounded-lg text-white text-sm w-100 h-15">Login</button>
+                </div>
+                <p className="flex justify-center gap-2">Do you have an Account?<Link to={"/Vagas"}>Register</Link></p>
+            </div>
+            <img src={photoLogin} alt="PhotoLogin" />
         </section>
     )
 }
--- Frontend/TicFrontend/src/components/input.jsx
+++ Frontend/TicFrontend/src/components/input.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Input(props) {
     return (
-        <input className="bg-white border-2 px-6 rounded-xl text-black text-sm w-100 h-15" placeholder="Enter your user name"></input>
+        <input className="bg-white border-2 px-6 rounded-lg text-black text-sm w-100 h-15" placeholder={props.placeholder}></input>
     )
 }
 
