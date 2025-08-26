d16291b08b955200493e851fd53a24b29eaf79af
Fernando Moreti <fernandombolela@gmail.com>
2025-08-26 00:53:28 -0300

    Deixando o login responsivo

--- Frontend/TicFrontend/src/Pages/Login.jsx
+++ Frontend/TicFrontend/src/Pages/Login.jsx
@@ -20,8 +20,8 @@ function Login() {
     }, []);
 
     return (
-        <section className="flex justify-center items-center">
-            <div className="flex flex-col w-125 h-150 border-1 rounded-xl justify-center p-10">
+        <section className="flex justify-center items-center gap-10">
+            <div className="flex flex-col w-90 md:w-125 h-150 border-1 rounded-xl justify-center p-5 md:p-10">
                 <div>
                     <h1 className="!text-2xl pb-8">Welcome !</h1>
                 </div>
@@ -30,17 +30,21 @@ function Login() {
                     <p className="text-sm pb-12">Lorem ipsum dolor</p>
                 </div>
                 <div className="pb-12">
-                    <p className="pb-2">User name</p>
-                    <Input placeholder="Enter your user name"></Input>
-                    <p className="pb-2">Password</p>
-                    <Input placeholder="Enter your password"></Input>
+                    <div className="pb-10">
+                        <p className="text-lg pb-2">User name</p>
+                        <Input placeholder="Enter your user name"></Input>
+                    </div>
+                    <div>
+                        <p className="text-lg pb-2">Password</p>
+                        <Input placeholder="Enter your password"></Input>
+                    </div>
                 </div>
                 <div className="pb-8">
-                    <button className="!bg-black border-2 px-6 rounded-lg text-white text-sm w-100 h-15">Login</button>
+                    <button className="!bg-black border-2 px-6 rounded-lg text-white text-lg w-80 md:w-100 h-15">Login</button>
                 </div>
-                <p className="flex justify-center gap-2">Do you have an Account?<Link to={"/Vagas"}>Register</Link></p>
+                <p className="flex justify-center gap-2">Do you have an Account?<Link className="!text-black" to={"/Vagas"}>Register</Link></p>
             </div>
-            <img src={photoLogin} alt="PhotoLogin" />
+            <img className="hidden md:flex" src={photoLogin} alt="PhotoLogin" />
         </section>
     )
 }
