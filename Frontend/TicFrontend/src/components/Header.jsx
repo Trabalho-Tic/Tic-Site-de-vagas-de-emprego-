1a013ed2de23fa4952d1d5feed3967483b115fcb
Fernando Moreti <fernandombolela@gmail.com>
2025-08-27 02:38:58 -0300

    Deixando o header responsivo

--- Frontend/TicFrontend/src/components/Header.jsx
+++ Frontend/TicFrontend/src/components/Header.jsx
@@ -6,15 +6,15 @@ import logo from "../assets/Jobior.png"
 function Home() {
 
     return (
-        <header className="flex justify-between items-center px-30 bg-gradient-to-t from-white to-gray-200 h-15">
-            <img className="h-5" src={logo} alt="" />
-            <nav className="flex gap-15">
+        <header className="flex flex-col sm:flex-row justify-between items-center sm:px-10 lg:px-30 bg-gradient-to-t from-white to-gray-200 h-15">
+            <img className="h-5 hidden sm:flex" src={logo} alt="" />
+            <nav className="flex py-5 sm:py-0 gap-8 sm:gap-15">
                 <Link to={"/"} className="text-lg font-medium transition-all duration-400 hover:text-gray-200">Vagas</Link>
                 <Link to={"/"} className="text-lg font-medium transition-all duration-400 hover:text-gray-200">Empresas</Link>
                 <Link to={"/"} className="text-lg font-medium transition-all duration-400 hover:text-gray-200">Profile</Link>
                 <Link to={"/"} className="text-lg font-medium transition-all duration-400 hover:text-gray-200">About</Link>
             </nav>
-            <Link className="bg-black text-white px-5 py-1 transition-all duration-500 hover:bg-gray-500 rounded-xl">Profile</Link>
+            <Link className="hidden sm:flex bg-black text-white px-5 py-1 transition-all duration-500 hover:bg-gray-500 rounded-xl">Profile</Link>
         </header>
     )
 }
