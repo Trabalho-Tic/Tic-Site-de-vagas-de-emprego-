111ca48c33afbed81e4ecb8a2cd32152ba79b4d1
Fernando Moreti <fernandombolela@gmail.com>
2025-08-27 02:30:49 -0300

    Deixando o search responsivo

--- Frontend/TicFrontend/src/Pages/Home.jsx
+++ Frontend/TicFrontend/src/Pages/Home.jsx
@@ -8,16 +8,16 @@ function Vagas() {
     return (
         <>
             <Header />
-            <section className="flex flex-col justify-center items-center gap-10 py-10">
+            <section className="flex flex-col justify-center items-center gap-10 pt-10">
                 <p className="text-4xl text-shadow-lg/10">Procure por um Job</p>
-                <div className="flex gap-5">
+                <div className="flex flex-col items-center lg:flex-row gap-5">
                     <Input placeholder="Procure pela Vaga"></Input>
                     <Input placeholder="Procure pela Localização"></Input>
-                    <button className="bg-gradient-to-t from-gray-50 to-gray-500 p-5 rounded-4xl transition-all duration-400 shadow-xl hover:-translate-y-1"><Search size={20} /></button>
+                    <button className="w-15 items-center bg-gradient-to-t from-gray-50 to-gray-500 p-5 rounded-4xl transition-all duration-400 shadow-xl hover:-translate-y-1"><Search size={20} /></button>
                 </div>
             </section>
             <section className="bg-gradient-to-t to-white from-gray-100 sm:px-0 lg:px-25 py-15 flex flex-col items-center">
-                <p className="text-3xl font-semibold pb-6">Vagas abertas recentemente</p>
+                <p className="text-3xl text-center font-semibold px-1 pb-6">Vagas abertas recentemente</p>
                 <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5">
                     <Card  />
                     <Card  />
