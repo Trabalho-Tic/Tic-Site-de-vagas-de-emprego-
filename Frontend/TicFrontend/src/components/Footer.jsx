8fb73561b9d6872481cd061fbe5cca42d53a4bdc
Fernando Moreti <fernandombolela@gmail.com>
2025-09-03 01:22:22 -0300

    Deixando o fotter responsivo

--- Frontend/TicFrontend/src/components/Footer.jsx
+++ Frontend/TicFrontend/src/components/Footer.jsx
@@ -4,10 +4,10 @@ import insta from "../assets/instagram.png"
 function Footer() {
 
     return (
-        <footer className="flex border-t-2 border-gray-300 px-27 items-center py-5">
+        <footer className="flex flex-col lg:flex-row border-t-2 border-gray-300 lg:px-27 items-center py-5">
             <div className="flex gap-5">
                 <button className="border-2 rounded-4xl p-2 transition-all duration-300 hover:-translate-y-1 hover:shadow-lg hover:shadow-green-300">
-                    <img className="h-4" src={insta} alt="" />
+                    <img src={insta} alt="" />
                 </button>
                 <button className="border-2 rounded-4xl p-2 transition-all duration-300 hover:-translate-y-1 hover:shadow-lg hover:shadow-green-300">
                     <img src={insta} alt="" />
@@ -19,7 +19,7 @@ function Footer() {
                     <img src={insta} alt="" />
                 </button>
             </div>
-            <div className="absolute left-1/2 font-semibold text-lg transform -translate-x-1/2 p-4">
+            <div className="lg:absolute lg:left-1/2 font-semibold text-lg lg:transform lg:-translate-x-1/2 p-4">
                 <p>Desenvolvido por alunos da Uni-Facef</p>
             </div>
         </footer>
