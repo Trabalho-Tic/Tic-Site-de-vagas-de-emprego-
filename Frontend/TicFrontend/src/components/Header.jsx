c8d234a89efc4cea402823944130cb006feb5b49
Fernando Moreti <fernandombolela@gmail.com>
2025-09-09 09:56:54 -0300

    fix(header) Ajustando o nav

--- Frontend/TicFrontend/src/components/Header.jsx
+++ Frontend/TicFrontend/src/components/Header.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 import "../Styles/home.css"
-import { Link } from "react-router-dom"
+import { NavLink, Link } from "react-router-dom"
 import logo from "../assets/Jobior.png"
 
 function Home() {
@@ -9,10 +9,10 @@ function Home() {
         <header className="flex flex-col sm:flex-row justify-between items-center sm:px-10 lg:px-30 bg-gradient-to-t from-white to-gray-200 h-15">
             <img className="h-5 hidden sm:flex" src={logo} alt="" />
             <nav className="flex py-5 sm:py-0 gap-8 sm:gap-15">
-                <Link to={"/"} className="text-lg font-medium transition-all duration-400 hover:text-gray-200">Vagas</Link>
-                <Link to={"/"} className="text-lg font-medium transition-all duration-400 hover:text-gray-200">Empresas</Link>
-                <Link to={"/"} className="text-lg font-medium transition-all duration-400 hover:text-gray-200">Profile</Link>
-                <Link to={"/"} className="text-lg font-medium transition-all duration-400 hover:text-gray-200">About</Link>
+                <NavLink to={"/vagas"} className={({ isActive }) => isActive ? "text-lg bg-green-300 rounded-2xl py-2 px-3 font-medium transition-all duration-400 hover:text-gray-200" : "text-lg font-medium py-2 px-3 transition-all duration-400 hover:text-gray-200"}>Vagas</NavLink>
+                <NavLink to={"/"} className={({ isActive }) => isActive ? "text-lg bg-green-300 rounded-4xl py-2 px-3 font-medium transition-all duration-400 hover:text-gray-200" : "text-lg font-medium py-2 px-3 transition-all duration-400 hover:text-gray-200"}>Empresas</NavLink>
+                <NavLink to={"/"} className={({ isActive }) => isActive ? "text-lg bg-green-300 rounded-4xl py-2 px-3 font-medium transition-all duration-400 hover:text-gray-200" : "text-lg font-medium py-2 px-3 transition-all duration-400 hover:text-gray-200"}>Profile</NavLink>
+                <NavLink to={"/"} className={({ isActive }) => isActive ? "text-lg bg-green-300 rounded-4xl py-2 px-3 font-medium transition-all duration-400 hover:text-gray-200" : "text-lg font-medium py-2 px-3 transition-all duration-400 hover:text-gray-200"}>About</NavLink>
             </nav>
             <Link className="hidden sm:flex bg-black text-white px-5 py-1 transition-all duration-500 hover:bg-gray-500 rounded-xl">Profile</Link>
         </header>
