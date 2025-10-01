a18fe497fbe60d1cc49a93de4b2a623c0b991807
Your Name <you@example.com>
2025-10-01 20:30:40 -0300

    feat(adicionando controller de admin e verificaçao por tipo de usuario

---
+++ Backend/src/controllers/adminAuthController.js
@@ -0,0 +1,46 @@
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const SECRET = process.env.JWT_SECRET; // Agora vem direto do .env
+
+class AuthController {
+    async login(req, res) {
+        const { email, password } = req.body;
+
+        try {
+            const user = await User.findOne({ where: { email } });
+            if (!user) {
+                return res.status(401).json({ error: "Usuário não encontrado" });
+            }
+
+            const validPassword = await bcrypt.compare(password, user.password);
+            if (!validPassword) {
+                return res.status(401).json({ error: "Senha incorreta" });
+            }
+
+            // Gerar o token com a role do usuário (tipo do usuário)
+            const token = jwt.sign(
+                { 
+                    id: user.id, 
+                    email: user.email, 
+                    typeUser: user.typeUser // Adiciona o tipo de usuário (admin, user, etc.)
+                },
+                SECRET,
+                { expiresIn: "2h" }
+            );
+
+            return res.json({
+                token,
+                user: { id: user.id, nome: user.nome, email: user.email, typeUser: user.typeUser }
+            });
+        } catch (error) {
+            return res.status(500).json({
+                error: "Erro no login",
+                details: error.message
+            });
+        }
+    }
+}
+
+module.exports = new AuthController();
--- Backend/src/middlewares/authMiddleware.js
+++ Backend/src/middlewares/authMiddleware.js
@@ -2,6 +2,7 @@ const jwt = require("jsonwebtoken");
 
 const SECRET = process.env.JWT_SECRET;
 
+// Middleware de autenticação
 function authMiddleware(req, res, next) {
     const authHeader = req.headers["authorization"];
     if (!authHeader) {
@@ -15,11 +16,20 @@ function authMiddleware(req, res, next) {
 
     try {
         const decoded = jwt.verify(token, SECRET);
-        req.user = decoded; // anexamos o usuário no request
+        req.user = decoded; // Anexa o usuário ao request
         next();
     } catch (error) {
         return res.status(401).json({ error: "Token inválido ou expirado" });
     }
 }
 
-module.exports = authMiddleware;
+
+function adminAuthMiddleware(req, res, next) {
+    
+    if (req.user.role !== "admin") {
+        return res.status(403).json({ error: "Acesso restrito a administradores" });
+    }
+    next()
+}
+
+module.exports = { authMiddleware, adminAuthMiddleware };
