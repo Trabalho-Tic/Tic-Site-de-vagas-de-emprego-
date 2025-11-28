b57832369cf8afb1e9dc237302a775446f24ca29
Fernando Moreti <fernandombolela@gmail.com>
2025-11-28 00:58:21 -0300

    feat(Admin) Adicionando NAV para mesclar nas pages

--- Frontend/TicFrontend/src/admin/pages/VinculoPage.jsx
+++ Frontend/TicFrontend/src/admin/pages/VinculoPage.jsx
@@ -1,16 +1,19 @@
 import React, { useState, useEffect } from "react";
 import useApi from "../../api/Api";
 import Checkbox from "../../components/Checkbox";
+import { NavLink } from "react-router-dom"
 
 function Vinculo() {
     const [tipoDeficiencias, setTipoDeficiencias] = useState([])
     const [tipoDeficienciasSelecionada, setTipoDeficienciaSelecionada] = useState(null)
     const [subTipoDeficiencias, setSubTipoDeficiencias] = useState([])
+    const [subTipoDeficienciaSelecionada, setSubTipoDeficienciaSelecionada] = useState(null)
     const [barreira, setBarreira] = useState("")
     const [barreiras, setBarreiras] = useState([])
     const [acessibilidades, setAcessibilidades] = useState([])
 
     const [update, setUpdate] = useState(0)
+    const [aba, setAba] = useState("tipo")
 
     useEffect(() => {
         async function fetchAll() {
@@ -79,79 +82,156 @@ function Vinculo() {
             </div>
 
             <div className="bg-white p-6 rounded-lg shadow-sm mb-8 border border-gray-200">
-                <h2 className="text-xl font-semibold mb-3">Tipo Deficiencia</h2>
-                <div className="flex flex-col gap-2">
-                    <p>Adicione um Tipo de Deficiência:</p>
-                    <div className="flex gap-5">
-                        <input type="text" className="w-full bg-gray-100 border-none rounded-md pl-10 pr-4 py-2 focus:ring-2 focus:ring-indigo-500" />
-                        <button className="bg-indigo-600 text-white px-8 py-4 rounded-md flex items-center gap-2 hover:bg-indigo-500">Criar</button>
-                    </div>
-                </div>
+                <nav className="flex justify-around">
+                    <NavLink
+                        onClick={() => setAba("tipo")}
+                        className={() =>
+                            `flex items-center gap-3 px-3 py-2 rounded-md hover:bg-gray-100 ${
+                                aba === "tipo" ? "bg-indigo-50 text-indigo-600 font-semibold" : ""
+                            }`
+                        }
+                    >
+                        Tipo Deficiencia
+                    </NavLink>
+                    <NavLink
+                        onClick={() => setAba("subtipo")}
+                        className={() =>
+                            `flex items-center gap-3 px-3 py-2 rounded-md hover:bg-gray-100 ${
+                                aba === "subtipo" ? "bg-indigo-50 text-indigo-600 font-semibold" : ""
+                            }`
+                        }
+                    >
+                        Sub Tipo Deficiencia
+                    </NavLink>
+                    <NavLink
+                        onClick={() => setAba("barreira")}
+                        className={() =>
+                            `flex items-center gap-3 px-3 py-2 rounded-md hover:bg-gray-100 ${
+                                aba === "barreira" ? "bg-indigo-50 text-indigo-600 font-semibold" : ""
+                            }`
+                        }
+                    >
+                        Barreira
+                    </NavLink>
+                    <NavLink
+                        onClick={() => setAba("acessibilidade")}
+                        className={() =>
+                            `flex items-center gap-3 px-3 py-2 rounded-md hover:bg-gray-100 ${
+                                aba === "acessibilidade" ? "bg-indigo-50 text-indigo-600 font-semibold" : ""
+                            }`
+                        }
+                    >
+                        Acessibilidade
+                    </NavLink>
+                </nav>
             </div>
 
-            <div className="grid grid-cols-4 gap-5 bg-white p-6 rounded-lg shadow-sm mb-8 border border-gray-200">
-                {
-                    tipoDeficiencias?.map((deficiencia) => (
-                        <div className="flex gap-2 items-center">
-                            <Checkbox 
-                                label={deficiencia.nome}
-                                checked={tipoDeficienciasSelecionada === deficiencia.nome}
-                                onChange={() => setTipoDeficienciaSelecionada(deficiencia.nome)}
-                            />
+            {aba === "tipo" && (
+                <>
+                    <div className="bg-white p-6 rounded-lg shadow-sm mb-8 border border-gray-200">
+                        <h2 className="text-xl font-semibold mb-3">Tipo Deficiencia</h2>
+                        <div className="flex flex-col gap-2">
+                            <p>Adicione um Tipo de Deficiência:</p>
+                            <div className="flex gap-5">
+                                <input type="text" className="w-full bg-gray-100 border-none rounded-md pl-10 pr-4 py-2 focus:ring-2 focus:ring-indigo-500" />
+                                <button className="bg-indigo-600 text-white px-8 py-4 rounded-md flex items-center gap-2 hover:bg-indigo-500">Criar</button>
+                            </div>
                         </div>
-                    ))
-                }
-            </div>
-
-
-            <div className="bg-white p-6 rounded-lg shadow-sm mb-8 border border-gray-200">
-                <h2 className="text-xl font-semibold mb-3">Sub Tipo Deficiencia</h2>
-                <div className="flex flex-col gap-2">
-                    <p>Adicione um Sub Tipo de Deficiência:</p>
-                    <div className="flex gap-5">
-                        <input type="text" className="w-full bg-gray-100 border-none rounded-md pl-10 pr-4 py-2 focus:ring-2 focus:ring-indigo-500" />
-                        <button className="bg-indigo-600 text-white px-8 py-4 rounded-md flex items-center gap-2 hover:bg-indigo-500">Criar</button>
                     </div>
-                </div>
-            </div>
 
-            <div className="grid grid-cols-4 gap-5 bg-white p-6 rounded-lg shadow-sm mb-8 border border-gray-200">
-                {
-                    subTipoDeficiencias?.map((subDeficiencia) => (
-                        <div className="flex gap-2 items-center">
-                            <Checkbox 
-                                label={subDeficiencia.nome}
-                                onChange={() => setTipoDeficienciaSelecionada(subDeficiencia.nome)}
-                            />
+                    <div className="grid grid-cols-4 gap-5 bg-white p-6 rounded-lg shadow-sm mb-8 border border-gray-200">
+                        {
+                            tipoDeficiencias?.map((deficiencia) => (
+                                <div className="flex gap-2 items-center">
+                                    <Checkbox 
+                                        label={deficiencia.nome}
+                                        checked={tipoDeficienciasSelecionada === deficiencia.nome}
+                                        onChange={() => setTipoDeficienciaSelecionada(deficiencia.nome)}
+                                    />
+                                </div>
+                            ))
+                        }
+                    </div>
+
+                    <div className="bg-white p-6 rounded-lg shadow-sm mb-8 border border-gray-200">
+                        <h2 className="text-xl font-semibold mb-3">Sub Tipo Deficiencia</h2>
+                        <div className="flex flex-col gap-2">
+                            <p>Adicione um Sub Tipo de Deficiência:</p>
+                            <div className="flex gap-5">
+                                <input type="text" className="w-full bg-gray-100 border-none rounded-md pl-10 pr-4 py-2 focus:ring-2 focus:ring-indigo-500" />
+                                <button className="bg-indigo-600 text-white px-8 py-4 rounded-md flex items-center gap-2 hover:bg-indigo-500">Criar</button>
+                            </div>
                         </div>
-                    ))
-                }
-            </div>
+                    </div>
+
+                    <div className="grid grid-cols-4 gap-5 bg-white p-6 rounded-lg shadow-sm mb-8 border border-gray-200">
+                        {
+                            subTipoDeficiencias?.map((subDeficiencia) => (
+                                <div className="flex gap-2 items-center">
+                                    <Checkbox 
+                                        label={subDeficiencia.nome}
+                                        onChange={() => setTipoDeficienciaSelecionada(subDeficiencia.nome)}
+                                    />
+                                </div>
+                            ))
+                        }
+                    </div>
+                </>
+            )}
             
-            
-            <div className="bg-white p-6 rounded-lg shadow-sm mb-8 border border-gray-200">
-                <h2 className="text-xl font-semibold mb-3">Barreira</h2>
-                <div className="flex flex-col gap-2">
-                    <p>Adicione uma  Barreira:</p>
-                    <div className="flex gap-5">
-                        <input type="text" value={barreira} onChange={(e) => setBarreira(e.target.value)} className="w-full bg-gray-100 border-none rounded-md pl-10 pr-4 py-2 focus:ring-2 focus:ring-indigo-500" />
-                        <button onClick={() => handleBarreira(event)} className="bg-indigo-600 text-white px-8 py-4 rounded-md flex items-center gap-2 hover:bg-indigo-500">Criar</button>
-                    </div>
-                </div>
-            </div>
+            {aba === "subtipo" && (
+                <>
 
-            <div className="grid grid-cols-4 gap-5 bg-white p-6 rounded-lg shadow-sm mb-8 border border-gray-200">
-                {
-                    barreiras?.map((barreira) => (
-                        <div className="flex gap-2 items-center">
-                            <Checkbox 
-                                label={barreira.descricao}
-                                onChange={() => setTipoDeficienciaSelecionada(barreira.nome)}
-                            />
+                    <div className="bg-white p-6 rounded-lg shadow-sm mb-8 border border-gray-200">
+                        <h2 className="text-xl font-semibold mb-3">Sub Tipo Deficiencia</h2>
+                        <div className="flex flex-col gap-2">
+                            <p>Adicione um Sub Tipo de Deficiência:</p>
+                            <div className="flex gap-5">
+                                <input type="text" className="w-full bg-gray-100 border-none rounded-md pl-10 pr-4 py-2 focus:ring-2 focus:ring-indigo-500" />
+                                <button className="bg-indigo-600 text-white px-8 py-4 rounded-md flex items-center gap-2 hover:bg-indigo-500">Criar</button>
+                            </div>
                         </div>
-                    ))
-                }
-            </div>
+                    </div>
+
+                    <div className="grid grid-cols-4 gap-5 bg-white p-6 rounded-lg shadow-sm mb-8 border border-gray-200">
+                        {
+                            subTipoDeficiencias?.map((subDeficiencia) => (
+                                <div className="flex gap-2 items-center">
+                                    <Checkbox 
+                                        label={subDeficiencia.nome}
+                                        checked={subTipoDeficienciaSelecionada === subDeficiencia.nome}
+                                        onChange={() => setSubTipoDeficienciaSelecionada(subDeficiencia.nome)}
+                                    />
+                                </div>
+                            ))
+                        }
+                    </div>
+
+                    <div className="bg-white p-6 rounded-lg shadow-sm mb-8 border border-gray-200">
+                        <h2 className="text-xl font-semibold mb-3">Barreira</h2>
+                        <div className="flex flex-col gap-2">
+                            <p>Adicione uma  Barreira:</p>
+                            <div className="flex gap-5">
+                                <input type="text" value={barreira} onChange={(e) => setBarreira(e.target.value)} className="w-full bg-gray-100 border-none rounded-md pl-10 pr-4 py-2 focus:ring-2 focus:ring-indigo-500" />
+                                <button onClick={(event) => handleBarreira(event)} className="bg-indigo-600 text-white px-8 py-4 rounded-md flex items-center gap-2 hover:bg-indigo-500">Criar</button>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="grid grid-cols-4 gap-5 bg-white p-6 rounded-lg shadow-sm mb-8 border border-gray-200">
+                        {
+                            barreiras?.map((barreira) => (
+                                <div className="flex gap-2 items-center">
+                                    <Checkbox 
+                                        label={barreira.descricao}
+                                        onChange={() => setTipoDeficienciaSelecionada(barreira.nome)}
+                                    />
+                                </div>
+                            ))
+                        }
+                    </div>
+                </>
+            )}
         </>
     )
 }
